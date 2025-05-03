0270371e671569cc4def3ea1daf39b7f00b1e77f
jordanfrancis <jordanfrancis@mu-602292.dhcp.missouri.edu>
2025-05-03 12:35:25 -0500

    dog with pet extension

--- CS3330_Assignment5/src/main/java/model/Dog.java
+++ CS3330_Assignment5/src/main/java/model/Dog.java
@@ -1,5 +1,36 @@
 package model;
 
-public class Dog {
+public class Dog extends Pet{
+	
+	private String breed;
 
+	/*
+	 * uses the same outline from Pet but updates it to have the breed slot for dogs
+	 */
+	public Dog(int id, String name, int age, boolean adopted, String breed) {
+		super(id, name, "Dog", breed, age, adopted);
+		// TODO Auto-generated constructor stub
+		this.breed = breed;
+	}
+	
+	/*
+	 * obtains the breed
+	 */
+	public String getBreed() {
+		return breed;
+	}
+	
+	/*
+	 * assigns the breed
+	 */
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + String.format(", Breed: %s", breed);
+	}
+	
+	
 }
