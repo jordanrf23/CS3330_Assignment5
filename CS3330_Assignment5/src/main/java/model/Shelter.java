a49b5fbe4de0b98e93d3c0613de54721c505f0cc
jordanfrancis <jordanfrancis@mu-602292.dhcp.missouri.edu>
2025-05-03 12:50:00 -0500

    shelter updated with methods and comments

--- CS3330_Assignment5/src/main/java/model/Shelter.java
+++ CS3330_Assignment5/src/main/java/model/Shelter.java
@@ -1,5 +1,84 @@
 package model;
 
-public class Shelter {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Shelter<T extends Pet>{
+	private final List<T> pets;
+	
+	/*
+	 * Aligning pets into a new list
+	 */
+	public Shelter() {
+		this.pets = new ArrayList<>();
+	}
+	
+	/*
+	 * add pet to the shelter's list
+	 */
+	public void addPet(T pet) {
+		pets.add(pet);
+	}
+	
+	/*
+	 * removes pet by using their id 
+	 */
+	public boolean removePetById(int id) {
+		return pets.removeIf(p-> p.getId() == id);
+	}
+	
+	/*
+	 * obtains a pet by their id 
+	 */
+	public T getPetById(int id) {
+		for(T pet: pets) {
+			if(pet.getId() == id) {
+				return pet;
+			}
+		}
+		return null;
+	}
+	
+	/*
+	 * marks pet as adopted if not already
+	 */
+	public boolean adpotPet(int id) {
+		T pet = getPetById(id);
+		if(pet != null && !pet.isAdopted()) {
+			pet.adopt();
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	 * sorts the pets naturally
+	 */
+	public void sortByName() {
+		Collections.sort(pets);
+	}
+	
+	/*
+	 * sorts with comparable 
+	 */
+	public void sort (Comparator<T> comparator) {
+		pets.sort(comparator);
+	}
+	
+	/*
+	 * returns a list of all the pets
+	 */
+	public List<T> getAllPets(){
+		return new ArrayList<>(pets);
+	}
+	
+	/*
+	 * clears all the pets
+	 */
+	public void clear() {
+		pets.clear();
+	}
 
 }
