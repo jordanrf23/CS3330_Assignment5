c1f80797bcb54bbe67c5ff496e1f324001e965d6
jordanfrancis <jordanfrancis@mu-602292.dhcp.missouri.edu>
2025-05-03 16:56:50 -0500

    updated gson in pom and finishing the comparators

--- CS3330_Assignment5/src/main/java/model/PetByName.java
+++ CS3330_Assignment5/src/main/java/model/PetByName.java
@@ -5,8 +5,7 @@ import java.util.Comparator;
 public class PetByName implements Comparator<Pet>{
 
 	@Override
-	public int compare(Pet o1, Pet o2) {
-		return;
+	public int compare(Pet p1, Pet p2) {
+		return p1.getName().compareToIgnoreCase(p2.getName());
 	}
-
 }
--- CS3330_Assignment5/src/main/java/model/PetBySpecies.java
+++ CS3330_Assignment5/src/main/java/model/PetBySpecies.java
@@ -1,5 +1,12 @@
 package model;
 
-public class PetBySpecies {
+import java.util.Comparator;
+
+public class PetBySpecies implements Comparator<Pet>{
+
+	@Override
+	public int compare(Pet arg0, Pet arg1) {
+		return arg0.getspecies().compareToIgnoreCase(arg1.getspecies());
+	}
 
 }
