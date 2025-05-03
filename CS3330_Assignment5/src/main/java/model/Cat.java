44abce66f8001daa0a9cdfe7972bebb00146dcda
jordanfrancis <jordanfrancis@mu-602292.dhcp.missouri.edu>
2025-05-03 12:37:53 -0500

    repeated implementation of Dog for cat and rabbit

--- CS3330_Assignment5/src/main/java/model/Cat.java
+++ CS3330_Assignment5/src/main/java/model/Cat.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Cat {
+public class Cat extends Pet{
+	
+	private String breed;
+
+	/*
+	 * uses the same outline from Pet but updates it to have the breed slot for cats
+	 */
+	public Cat(int id, String name, int age, boolean adopted, String breed) {
+		super(id, name, "Cat", breed, age, adopted);
+		// TODO Auto-generated constructor stub
+		this.breed = breed;
+	}
+	
+	/*
+	 * obtains the breed
+	 */
+	public String getBreed() {
+		return breed;
+	}
+	
+	/*
+	 * assigns the breed
+	 */
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + String.format(", Breed: %s", breed);
+	}
 
 }
--- CS3330_Assignment5/src/main/java/model/Rabbit.java
+++ CS3330_Assignment5/src/main/java/model/Rabbit.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Rabbit {
+public class Rabbit extends Pet{
+	
+	private String breed;
+
+	/*
+	 * uses the same outline from Pet but updates it to have the breed slot for Rabbits
+	 */
+	public Rabbit(int id, String name, int age, boolean adopted, String breed) {
+		super(id, name, "Rabbit", breed, age, adopted);
+		// TODO Auto-generated constructor stub
+		this.breed = breed;
+	}
+	
+	/*
+	 * obtains the breed
+	 */
+	public String getBreed() {
+		return breed;
+	}
+	
+	/*
+	 * assigns the breed
+	 */
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + String.format(", Breed: %s", breed);
+	}
 
 }
