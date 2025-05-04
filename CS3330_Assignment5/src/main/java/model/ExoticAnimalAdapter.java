73d194bf5034a5bbbdb9e306b0a3d34049bdd250
jordanfrancis <jordanfrancis@mu-602292.dhcp.missouri.edu>
2025-05-04 02:52:35 -0500

    explaining the code

--- CS3330_Assignment5/src/main/java/model/ExoticAnimalAdapter.java
+++ CS3330_Assignment5/src/main/java/model/ExoticAnimalAdapter.java
@@ -4,28 +4,42 @@ public class ExoticAnimalAdapter extends Pet {
 	
 	private ExoticAnimal exoticAnimal;
 
+	/*
+	 * uses the pet method and makes in comparable with exotic
+	 */
 	public ExoticAnimalAdapter(String id, ExoticAnimal exoticAnimal) {
 		super(id, exoticAnimal.getAnimalName(), exoticAnimal.getAnimalType(), "Exotic", exoticAnimal.getYearsOld(), exoticAnimal.isAdopted());
 		this.exoticAnimal = exoticAnimal;
 	}
 	
+	/*
+	 * obtains id from exotic
+	 */
 	 @Override
 	    public String getId() {
 	        return exoticAnimal.getUniqueId();
 	    }
 	
+	 /*
+	  * obtains the name from exotic
+	  */
 	@Override
 	public String getName() {
 		return exoticAnimal.getAnimalName();
 	}
 
-
+	/*
+	 * changes the adopted mark
+	 */
 	@Override
 	 public void adopt() {
 	     exoticAnimal.markAsAdopted();
 	     this.setAdopted(true);
 	}
-
+	
+	/*
+	 * returns the exotic animal given it all
+	 */
 	 public ExoticAnimal getExoticAnimal() {
 		 return exoticAnimal;
 	 }
