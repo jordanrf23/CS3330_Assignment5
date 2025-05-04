bae8b118d15d54934c4a4f2f4f7c9c0718b8ebc7
jordanfrancis <jordanfrancis@mu-602292.dhcp.missouri.edu>
2025-05-04 02:49:48 -0500

    explaining comparators in code

--- CS3330_Assignment5/src/main/java/model/PetByName.java
+++ CS3330_Assignment5/src/main/java/model/PetByName.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 
 public class PetByName implements Comparator<Pet>{
 
+	/*
+	 * comparing two names at a time 
+	 */
 	@Override
 	public int compare(Pet p1, Pet p2) {
 		return p1.getName().compareToIgnoreCase(p2.getName());
--- CS3330_Assignment5/src/main/java/model/PetBySpecies.java
+++ CS3330_Assignment5/src/main/java/model/PetBySpecies.java
@@ -4,6 +4,9 @@ import java.util.Comparator;
 
 public class PetBySpecies implements Comparator<Pet>{
 
+	/*
+	 * compares two species at a time 
+	 */
 	@Override
 	public int compare(Pet arg0, Pet arg1) {
 		return arg0.getspecies().compareToIgnoreCase(arg1.getspecies());
