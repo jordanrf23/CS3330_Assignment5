fdc8a885ca772891f13a363d5d81684d4382e061
jordanfrancis <jordanfrancis@mu-602292.dhcp.missouri.edu>
2025-05-04 02:54:58 -0500

    commenting the methods

--- CS3330_Assignment5/src/main/java/model/ExoticAnimal.java
+++ CS3330_Assignment5/src/main/java/model/ExoticAnimal.java
@@ -7,6 +7,9 @@ public class ExoticAnimal {
 	private String animalType;
 	private boolean isAdopted;
 	
+	/*
+	 * based on the json file each attribute
+	 */
 	public ExoticAnimal(String animalName, int yearsOld, String animalType, boolean isAdopted) {
 		super();
 		this.animalName = animalName;
@@ -15,28 +18,44 @@ public class ExoticAnimal {
 		this.isAdopted = isAdopted;
 	}
 	
-
+	/*
+	 * obtains the id
+	 */
 	public String getUniqueId() {
 		return uniqueId;
 	}
 
-
+	/*
+	 * obtains the name
+	 */
 	public String getAnimalName() {
 		return animalName;
 	}
 
+	/*
+	 * obtains the age
+	 */
 	public int getYearsOld() {
 		return yearsOld;
 	}
-
+	
+	/*
+	 * obtains the type
+	 */
 	public String getAnimalType() {
 		return animalType;
 	}
 
+	/*
+	 * updates the adoption
+	 */
 	public boolean isAdopted() {
 		return isAdopted;
 	}
 	
+	/*
+	 * changes the adoption attribute
+	 */
 	public void markAsAdopted() {
 		this.isAdopted = true;
 	}
